f723155fbddf7264b9628972e34ade9fa26916cb
khannaanurag1 <khannaanurag1@gmail.com>
2026-02-04 09:10:07 +0530

    Integrating Redis

--- src/main/java/org/example/productcatalogservice_nov2025morning/services/FakeStoreProductService.java
+++ src/main/java/org/example/productcatalogservice_nov2025morning/services/FakeStoreProductService.java
@@ -7,6 +7,8 @@ import org.example.productcatalogservice_nov2025morning.models.Category;
 import org.example.productcatalogservice_nov2025morning.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +21,10 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
+@Primary
 public class FakeStoreProductService implements IProductService {
 
     @Autowired
@@ -29,9 +33,32 @@ public class FakeStoreProductService implements IProductService {
     @Autowired
     private FakeStoreApiClient fakeStoreApiClient;
 
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
     @Override
     public Product getProductById(Long id) {
-        FakeStoreProductDto fakeStoreProductDto = fakeStoreApiClient.getProductById(id);
+
+//        if found in redis
+//                 return from redis
+//        else
+//            make call to fakestore
+//            store in redis
+//            return
+        FakeStoreProductDto fakeStoreProductDto = null;
+
+       fakeStoreProductDto =  (FakeStoreProductDto)
+               redisTemplate.opsForHash().get("PRODUCTS",id);
+
+       if(fakeStoreProductDto == null) {
+           fakeStoreProductDto = fakeStoreApiClient.getProductById(id);
+           redisTemplate.opsForHash().put("PRODUCTS",id,fakeStoreProductDto);
+           redisTemplate.expire("PRODUCTS", 5, TimeUnit.MINUTES);
+           System.out.println("Found by calling fakestore");
+       } else {
+           System.out.println("Found in redis cache");
+       }
+
 
         if (fakeStoreProductDto != null) {
          return from(fakeStoreProductDto);
